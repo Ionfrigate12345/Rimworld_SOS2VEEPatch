79fe2caeaf4d0bf8b27135a4e9dc161316815907
Ionfrigate12345 <alfred.shi0404@gmail.com>
2024-06-07 16:28:32 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+This patch will prevent Vanilla Helixien Gas Expanded (VHGE) from spawning geysers on SOS2 space maps, therefore removing the incompatibility between two mods.
+
+I will request the authors of SOS2 to remove the incompatible warning against VHGE but with this patch you can just ignore it.
+
+Need to be loaded after both SOS2 and VHGE
+
+Harmony required.
+
+
+该补丁阻止VE天然气拓展在SOS2的太空地图生成矿点，从而解决了两个MOD不兼容的主要问题。
+
+我后面会找SOS2的作者请求去除针对VE天然气拓展的不兼容警报，但即使有警报，有了该补丁也可以无视之。
+
+加载顺序必须在SOS2和VE天然气拓展之后。
+
+需要Harmony
+
+
+Ce patch empêchera l'apparition de geysers de gaz helixien de la part de Vanilla Helixien Gas Expanded (VHGE) sur les cartes spatiales SOS2, enlevant ainsi l'incompatibilité entre deux mods.
+
+Plus tard je demanderai aux auteurs de SOS2 d'enlever l'avertissement d'incompatibilité contre VHGE mais avant cela avec ce patch vous pouvez simplement ignorer cet avertissement dans votre liste de mods.
+
+Doit se charger après SOS2 et VHGE
+
+Harmony requis.
---
+++ Source/SOS2VEEPatch/HarmonyPatches.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using HarmonyLib;
+using RimWorld;
+using SaveOurShip2;
+using VEE.RegularEvents;
+using Verse;
+
+namespace SOS2VEEPatch
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        private static Harmony _harmonyInstance;
+        public static Harmony HarmonyInstance { get { return _harmonyInstance; } }
+
+        static HarmonyPatches()
+        {
+            _harmonyInstance = new Harmony("com.ionfrigate12345.sos2veepatch");
+            _harmonyInstance.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(VEE.PurpleEvents.PsychicRain))]
+        [HarmonyPatch("ForcedWeather")]
+        public static class PsychicRain_ForcedWeather_Patch
+        {
+            public static bool Prefix(VEE.PurpleEvents.PsychicRain __instance)
+            {
+                var affectedSOS2Maps = __instance.AffectedMaps.Where(map => HarmonyUtils.IsSOS2SpaceMap(map)).ToList();
+                if (affectedSOS2Maps.Count > 0)
+                {
+                    //Terminate psychic rain if it happens on any SOS2 space map.
+                    __instance.suppressEndMessage = true;
+                    __instance.End();
+                    Log.Warning("[SOS2VEEPatch] VEE Psychic Rain terminated on SOS2 space maps.");
+                    return false;
+                }
+                return true; 
+            }
+        }
+
+        [HarmonyPatch(typeof(VEE.PurpleEvents.PsychicRain))]
+        [HarmonyPatch("GameConditionTick")]
+        public static class PsychicRain_GameConditionTick_Patch
+        {
+            public static bool Prefix(VEE.PurpleEvents.PsychicRain __instance)
+            {
+                if (GenTicks.TicksAbs % 15 == 5) //Run less than once-per-tick for not causing performance issue.
+                {
+                    var affectedSOS2Maps = __instance.AffectedMaps.Where(map => HarmonyUtils.IsSOS2SpaceMap(map)).ToList();
+                    if (affectedSOS2Maps.Count > 0)
+                    {
+                        //Terminate psychic rain if it happens on any SOS2 space map.
+                        __instance.suppressEndMessage = true;
+                        __instance.End();
+                        Log.Warning("[SOS2VEEPatch] VEE Psychic Rain terminated on SOS2 space maps.");
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(CaravanAnimalWI))]
+        [HarmonyPatch("CanFireNowSub")]
+        public static class CaravanAnimalWI_CanFireNowSub_Patch
+        {
+            public static bool Prefix(ref bool __result, IncidentParms parms)
+            {
+                __result = false;
+                Log.Warning("[SOS2VEEPatch] VEE Caravan Animal Wander-In event banned for SOS2 balance (This event may give you archon animals).");
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(CaravanAnimalWI))]
+        [HarmonyPatch("TryExecuteWorker")]
+        public static class CaravanAnimalWI_TryExecuteWorker_Patch
+        {
+            public static bool Prefix(ref bool __result, IncidentParms parms)
+            {
+                __result = false;
+                Log.Warning("[SOS2VEEPatch] VEE Caravan Animal Wander-In event banned for SOS2 balance (This event may give you archon animals).");
+                return false;
+            }
+        }
+    }
+}
